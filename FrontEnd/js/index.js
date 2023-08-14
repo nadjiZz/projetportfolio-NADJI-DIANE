041c94c9b0d939fed8e68bfe17ba3cff588b2a31
nadjiZz <115700348+nadjiZz@users.noreply.github.com>
2023-08-14 20:25:07 +0100

    ajout de la parti retour et quitter la modale.

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -68,90 +68,57 @@ btnHotel.addEventListener("click", function(){
 // Recuperation du token
 const usertorage = window.localStorage.getItem("user");
 const token=window.localStorage.getItem("token");
-// localStorage.setItem('token',JSON.stringify('token'))
+localStorage.setItem('token',JSON.stringify('token'))
 if (usertorage && token) {
   const deconect = document.querySelector(".login")
   deconect.innerText = "logaout";
-
   const btnn = document.querySelector(".group");
-    btnn.style.display = "none";
-    const btn1 = document.querySelector("#myBtn1");
-    btn1.style.display = "block";
-    const btn2 = document.querySelector("#myBtn2");
-    btn2.style.display = "block";
-    const btn3 = document.querySelector("#myBtn3");
-    btn3.style.display = "block";
-    const editer =document.getElementById("edition");
-    editer.style.display = "flex";
-
-  deconect.addEventListener("logout", (event) => {
-    window.localStorage.removeItem("token")
-
-    if( token === null ){
-      console.log("valider teste")
-    } 
-  
-  });
-}
-
-//   deconect.addEventListener("click",function(){
-//   sessionStorage.removeItem('token');
-//   sessionStorage.removeItem('userId');
-//   document.location.href="index.html"
-//   deconect.style.displaye = "none"
-//   btnn.style.display = "block";
-//   btn1.style.display = "none";
-//   const btn2 = document.querySelector("#myBtn2");
-//   btn2.style.display = "none";
-//   const btn3 = document.querySelector("#myBtn3");
-//   btn3.style.display = "none";
-//   editer.style.display = "none";
-//   // localStorage.removeItem("token");
-//   })
-//     } else {
-//       const editer =document.getElementById("edition");
-//       editer.style.display = "none";
-//     }
+  btnn.style.display = "none";
+  const btn1 = document.querySelector("#myBtn1");
+  btn1.style.display = "block";
+  const btn2 = document.querySelector("#myBtn2");
+  btn2.style.display = "block";
+  const btn3 = document.querySelector("#myBtn3");
+  btn3.style.display = "block";
+  const editer =document.getElementById("edition");
+  editer.style.display = "flex";
 
 
-// const token = window.localStorage.getItem("token");
-// if (token) {
-//   const deconect = document.querySelector(".login")
-//   deconect.innerText = "logout";
-//   deconect.addEventListener("click",function(){
-//     window.localStorage.removeItem("token")
-//     deconect.removeAttribute('href')
-//     deconect.setAttribute('href','/FrontEnd/index.html')
-//   })
-//   const btnn = document.querySelector(".group");
-//   btnn.style.display = "none";
-//   const btn1 = document.querySelector("#myBtn1");
-//   btn1.style.display = "block";
-//   const btn2 = document.querySelector("#myBtn2");
-//   btn2.style.display = "block";
-//   const btn3 = document.querySelector("#myBtn3");
-//   btn3.style.display = "block";
-//   const editer =document.getElementById("edition");
-//   editer.style.display = "flex";
-//   // localStorage.removeItem("token");
-    
-//     } else {
-//       const editer =document.getElementById("edition");
-//       editer.style.display = "none";
-//     }
+      deconect.addEventListener("click",function(){
+  sessionStorage.removeItem('token');
+  sessionStorage.removeItem('userId');
+  document.location.href="index.html"
+  deconect.style.displaye = "none"
+  btnn.style.display = "block";
+  btn1.style.display = "none";
+  const btn2 = document.querySelector("#myBtn2");
+  btn2.style.display = "none";
+  const btn3 = document.querySelector("#myBtn3");
+  btn3.style.display = "none";
+  editer.style.display = "none";
+  localStorage.removeItem("token");
+  })
 
 
-
-// Get the modal
+// creation de la modale
 const modal = document.getElementById ("ensmble")
 const modall = document.getElementById("myModal");
 const modalll = document.getElementById("myModall")
-// Get the button that opens the modal
+// les bbouttons permettant d'ouvrir la moadale
 const Btn3 = document.getElementById("myBtn3")
 const btn4 = document.getElementById("ajoutphoto")
-// Get the <span> element that closes the modal
-const quitter = document.querySelectorAll(".close, .closer")[0];
-// When the user clicks on the button, open the modal
+// les bouttons permettant de fermer les modales
+const quitter = document.getElementById("close")
+const quitterr = document.getElementById("closer")
+// Quand on clique sur 'x' la modale se ferme
+quitter.onclick = function (){
+  modal.style.display = "none";
+
+}
+quitterr.onclick = function (){
+  modal.style.display = "none";
+}
+// Quand on clique sur une boutton modifier
 Btn3.onclick = function () {
   modal.style.display = "block";
   modall.style.display = "block";
@@ -162,11 +129,8 @@ btn4.onclick = function () {
   modall.style.display = "none";
   modalll.style.display = "block"
 }
-// When the user clicks on <span> (x), close the modal
-  quitter.addEventListener('click',function(x){
-  modal.style.display = "none";
-  })
-// When the user clicks anywhere outside of the modal, close it
+
+// quitter la modale en cliquant en dehors de la modale
 modal.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
@@ -178,13 +142,13 @@ window.addEventListener('keydown', function(e){
     modal.style.display = "none";
   }
 })
-const retour = document.getAnimations(".fa-arrow-left");
+// Cliquer sur la fleche pour révenir sur la premiere modale
+const retour = document.getElementById("left");
 retour.onclick = function () {
   modal.style.display = "block";
   modall.style.display = "block";
   modalll.style.display = "none"
 }
-console.log(retour)
 // Get the modal element
 for (let i = 0; i < portfolio.length; i++) {
   // Création des balises
@@ -194,8 +158,38 @@ for (let i = 0; i < portfolio.length; i++) {
   imageElement.src = portfolio[i].imageUrl;
   const nomElement = document.createElement("p");
   nomElement.innerText = "editer";
-  const iconsup = document.createElement("i");
-  iconsup.classList.add("fa-solid", "fa-trash-can");
+  const iconSup = document.createElement("i");
+  iconSup.classList.add("fa-solid", "fa-trash-can");
+
+
+   //  Envoie d'une requete pour supprimer l'element 
+ const iconSupProjet =  document.querySelectorAll(".fa-solid.fa-trash-can");
+ const portfolioId = portfolio.map(portfolio => portfolio.id);
+ for (let i = 0; i < portfolio.length; i++){
+   iconSupProjet.onclick = function () {            
+       fetch(`http://localhost:5678/api/works/${id}`, {
+       method: "DELETE", 
+       headers: {
+           "Content-type": "application/json",
+           "Authorization": "Bearer" + sessionStorage.getItem("token")
+           },
+       })
+       .then(response => {
+           if (response.ok) {
+             // Supprimer l'élément dans la modale dynamiquement
+             iconSupProjet.remove.cilck();
+             alert('photo supprimé avec succer')
+             // Supprimer l'élément dans la galerie dynamiquement
+             const work = document.getElementById(`work${i+1}`);
+             console.log(work);
+             work.replaceWith();
+           }
+           else {
+             alert("Echec de suppression");
+           }
+   });
+};
+};
 
   //  const workId = work.map(work => work.id);
   //     for (let i = 0; i < work.length; i++){
@@ -224,39 +218,14 @@ for (let i = 0; i < portfolio.length; i++) {
   //   };
   // };
 
+
+ 
+
   //Rattachement de nos balises au DOM
   sectionPortfolio.appendChild(sectionProjet);
   sectionProjet.appendChild(imageElement);
   sectionProjet.appendChild(nomElement);
-  sectionProjet.appendChild(iconsup);
+  sectionProjet.appendChild(iconSup);
+}
 }
 
-// Suppression d'une image de la galerie modale et de la base de données
-const GalerieModal = document.querySelector(".modal-content");
-
-GalerieModal.addEventListener('click', function(d){
-    // Verifier si l'element cliqué est une icone "Delete"
-    if(d.target.classList.contains('fa-trash-can')){
-        // Recuperation de l'element figure, parent correspondant
-        const figure = d.target.closest('figure');
-        // Récuperation de l'ID de l'element figure à supprimer
-        const workId = figure.getAttribute('id');
-        const getTokent = window.sessionStorage.getItem('token')
-        // Envoie d'une requete DELETE pour supprimer l'element 
-        fetch(`http://localhost:5678/api/works/${workId}`, {
-            method: 'DELETE',
-            headers: {"Authorization": `Bearer ${getTokent}`}
-        }).then(function(res){
-            if(res.ok){
-                //Affiche les projets de la gallerie modale
-                generationTravaux()
-                //Affiche les projets de la gallerie modale de le page d'accueil
-                genererGallerieModal()
-            }else{
-                console.error("Erreur survenue lors de la suppression de l\'element")
-            }
-        }).catch(function(error){
-            console.error("Erreur survenue lors de la suppression de l\'element", error)
-        })
-    }
-})
